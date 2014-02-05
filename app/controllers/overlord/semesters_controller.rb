712f58ab2eb617412e9537647830825e297a6a89
Nathaniel Watts <reg@nathanielwatts.com>
2014-02-04 23:15:14 -0700

    semester creation

--- app/controllers/overlord/semesters_controller.rb
+++ app/controllers/overlord/semesters_controller.rb
@@ -3,5 +3,25 @@ module Overlord
     def index
       @semesters = Semester.all
     end
+
+    def new
+      @semester = Semester.new
+    end
+
+    def create
+      @semester = Semester.new(semester_params)
+      if @semester.save
+        flash[:success] = "Successfully added semester #{@semester.name}."
+        redirect_to overlord_semesters_path
+      else
+        flash[:error] = "Oops, something went wrong."
+        render :new
+      end
+    end
+
+    private
+    def semester_params
+      params.require(:semester).permit(:name, :start_date, :end_date)
+    end
   end
 end
--- spec/features/overlord/semesters/organization_leader_can_create_a_semester_spec.rb
+++ spec/features/overlord/semesters/organization_leader_can_create_a_semester_spec.rb
@@ -8,10 +8,10 @@ feature "org leader" do
     click_on "Add Semester"
     name = "Fall 2014"
     fill_in "Name",       :with => name
-    fill_in "Start Date", :with => Time.current
-    fill_in "End Date",   :with => 3.months.from_now
+    fill_in "Start date", :with => Time.current
+    fill_in "End date",   :with => 3.months.from_now
     click_on "Create Semester"
 
-    expect(page).to have_content "Successfully added semester #{name}"
+    expect(page).to have_content "Successfully added semester #{name}."
   end
 end
