e5e9923704bddebe11785149befed5bf40f18279
Nathaniel Watts <reg@nathanielwatts.com>
2014-02-04 23:06:30 -0700

    add Semester

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140205045733) do
+ActiveRecord::Schema.define(version: 20140205060536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20140205045733) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+  end
+
+  create_table "semesters", force: true do |t|
+    t.string   "name"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
---
+++ spec/factories/semesters.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :semester do
+    name "MyString"
+    start_date "2014-02-04"
+    end_date "2014-02-04"
+  end
+end
