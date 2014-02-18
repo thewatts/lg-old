3642d6be4b0c5db1ebea225962c12358182a3838
Nathaniel Watts <reg@nathanielwatts.com>
2014-02-18 14:57:44 -0700

    tidy email settings for staging, add them for dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,12 @@
 Lg::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings   = {
+      :port    => '1025',
+      :address => 'localhost',
+      :domain  => 'lgapp',
+  }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,6 +1,17 @@
 Lg::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Mailer settings
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings   = {
+      :port           => '587',
+      :address        => 'smtp.mandrillapp.com',
+      :user_name      => ENV['MANDRILL_USERNAME'],
+      :password       => ENV['MANDRILL_APIKEY'],
+      :domain         => 'heroku.com',
+      :authentication => :plain
+  }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
