b350e97cc0b309b75df5695d94d2b8c58f7f8a3a
Nathaniel Watts <reg@nathanielwatts.com>
2014-02-18 18:27:54 -0700

    refactor sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,28 +3,37 @@ class SessionsController < ApplicationController
 
   def create
     user = User.from_omniauth(auth_hash) if auth_hash
-    if user
-      log_in_user(user)
-    else
-      flash[:error] = logging_in_error_message
-      redirect_to leader_signup_path
-    end
+    user ? log_in(user) : error_logging_in(user)
   end
 
   private
+
   def auth_hash
     request.env['omniauth.auth']
   end
 
-  def log_in_user(user)
+  def log_in(user)
     session[:user_id] = user.id
     if user.complete?
-      flash[:success] = logged_in_message_for(user)
-      redirect_to leader_dashboard_path(:nickname => user.nickname)
+      send_to_dashboard(user)
     else
-      flash[:success] = signed_up_message_for(user)
-      redirect_to leader_finish_signup_path(:nickname => user.nickname)
+      send_to_finish_signup(user)
     end
+  end
+
+  def error_logging_in(user)
+    flash[:error] = logging_in_error_message
+    redirect_to leader_signup_path
+  end
+
+  def send_to_dashboard(user)
+    flash[:success] = logged_in_message_for(user)
+    redirect_to leader_dashboard_path(:nickname => user.nickname)
+  end
+
+  def send_to_finish_signup(user)
+    flash[:success] = signed_up_message_for(user)
+    redirect_to leader_finish_signup_path(:nickname => user.nickname)
   end
 
   def signed_up_message_for(user)
@@ -38,6 +47,5 @@ class SessionsController < ApplicationController
   def logging_in_error_message
     "Sorry! Looks like there was an issue. Please try again or contact support."
   end
-
 
 end
