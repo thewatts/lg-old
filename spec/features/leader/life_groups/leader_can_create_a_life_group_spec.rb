aff4c0fea2095514facbd30b17b6a09a75a333b6
Nathaniel Watts <reg@nathanielwatts.com>
2014-02-22 18:49:04 -0700

    adjust group creation spec for basic_info step

--- spec/features/leader/life_groups/leader_can_create_a_life_group_spec.rb
+++ spec/features/leader/life_groups/leader_can_create_a_life_group_spec.rb
@@ -8,19 +8,22 @@ feature "lifegroups" do
     create(:semester, :name => "Fall 2014")
   end
 
-  scenario "can be created through the admin page" do
+  scenario "can be created with basic info" do
     visit leader_dashboard_path(:nickname => user.nickname)
-
     click_on "Create Group"
+    expect(page.current_url).to eq leader_groups_edit_basic_info_url(
+      :nickname => user.nickname)
+
     name = "New Breed"
     fill_in "Name",            :with => name
     select "Fall 2014",        :from => "Semester"
     fill_in "Description",     :with => "A small group of men."
     # https://gist.github.com/thijsc/1391107 -- for later
-    #select "Nathan Pruzaniec", :from => "Co Leaders"
-    #select "J Parker",         :from => "Co Leaders"
-    click_on "Create Life Group"
+    # select "Nathan Pruzaniec", :from => "Co Leaders"
+    # select "J Parker",         :from => "Co Leaders"
+    click_on "Save and Continue"
 
-    expect(page).to have_content "Thanks for submitting #{name} for approval!"
+    expect(page).to have_content "Specific Info for #{name}"
+    expect(page.current_url).to eq group_creation_specific_info_url
   end
 end
