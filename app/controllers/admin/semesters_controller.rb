0570d22ba8319ee1d660b7c78530de120b5f8b7b
Nathaniel Watts <reg@nathanielwatts.com>
2014-02-27 16:38:28 -0700

    remove duplication

--- app/controllers/admin/semesters_controller.rb
+++ app/controllers/admin/semesters_controller.rb
@@ -11,8 +11,7 @@ module Admin
     def create
       @semester = Semester.new(semester_params)
       if @semester.save
-        flash[:success] = "Successfully added semester #{@semester.name}."
-        redirect_to admin_semesters_path
+        send_to_dashboard
       else
         flash[:error] = "Oops, something went wrong."
         render :new
@@ -22,6 +21,11 @@ module Admin
     private
     def semester_params
       params.require(:semester).permit(:name, :start_date, :end_date)
+    end
+
+    def send_to_dashboard
+      flash[:success] = "Successfully added semester #{@semester.name}."
+      redirect_to admin_semesters_path
     end
   end
 end
